f0ecffb5f78fcebd6b3003f5eae3edfb535348d2
ZzqMa <buttlaface@gmail.com>
2026-02-08 22:35:36 -0600

    Delete toadscwm directory

--- toadscwm/install.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ~/.config/polybar
-cp ~/toadscwm/xinitrc ~/.xinitrc
-cp ~/toadscwm/cwmrc ~/.cwmrc
-cp ~/toadscwm/config.ini ~/.config/polybar/config.ini
-cp ~/toadscwm/Xresources ~/.Xresources
