082f56cb43866ec017f7db84aebd094df91669bd
ZzqMa <buttlaface@gmail.com>
2026-02-08 22:35:56 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.config/polybar
+cp ~/toadscwm/xinitrc ~/.xinitrc
+cp ~/toadscwm/cwmrc ~/.cwmrc
+cp ~/toadscwm/config.ini ~/.config/polybar/config.ini
+cp ~/toadscwm/Xresources ~/.Xresources
